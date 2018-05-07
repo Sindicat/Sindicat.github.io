f79d6aece3db2c0a6f83d30340cb1ecff24ab9f3
PaLLadiy46 <max_mart@icloud.com>
2018-05-08 00:46:12 +0300

    all functions for letters

--- confettiJS.js
+++ confettiJS.js
@@ -13,7 +13,7 @@ class Birthday {
         this.radius = 33
         this.currTxt = 0
         this.lastTime = 0
-        this.standarDelay = 3100
+        this.standarDelay = 3500
         this.delay = this.standarDelay
 
     }
